59d2cbac936693986aed74d58dbc8f9cd245bb8f
Shivam Palaskar <59534343+shivampalaskar@users.noreply.github.com>
2020-03-13 04:24:17 +0530

    Add files via upload

---
+++ Assignment1/DS_Assign1_Q8.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstring>
+using namespace std;
+
+void swap(int &str, int&str1) {
+	char temp = str;
+	str = str1;
+	str1 = temp;
+}
+
+// Bubble Sort
+string orderByAlphabet(string str) {
+	int len = str.length();
+	for (int i = 0; i < len; i++) {
+		for (int j = 0; j < len - 1; j++) {
+			if (str[j] > str[j + 1]) {
+				swap(str[j], str[j + 1]);
+			}
+		}
+	}
+	return str;
+}
+
+int main() {
+	string str;
+	cout << "Enter a String : "; // String without Spaces
+	cin >> str;
+	string sortedString = orderByAlphabet(str);
+	cout << "String Arranged By Alphabet : "<<sortedString;
+}
