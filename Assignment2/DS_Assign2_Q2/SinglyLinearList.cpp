e5c1bd5877bc59c8dfc1ba78d75bb65f59b15f5c
Shivam Palaskar <59534343+shivampalaskar@users.noreply.github.com>
2020-03-19 04:37:56 +0530

    Add files via upload

---
+++ Assignment2/DS_Assign2_Q2/SinglyLinearList.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include "SinglyLinearList.h"
+using namespace std;
+
+SinglyLinearList::SinglyLinearList() {
+	this->head = NULL;
+}
+
+Node* SinglyLinearList::getLastNode() {
+	Node *trav = head;
+	Node *lastNode;
+	while (trav != NULL) {
+		lastNode = trav;
+		trav = trav->nextNode;
+	}
+	return lastNode;
+}
+
+Node* SinglyLinearList::getLastNode_EffectiveMethod() {
+	Node *trav = head;
+	while (trav->nextNode != NULL) {
+		trav = trav->nextNode;
+	}
+	return trav;
+}
+
+void SinglyLinearList::getData(Node* newNode) {
+	int tempNum;
+	cout << "Enter Number  : ";
+	cin >> tempNum;
+	newNode->num = tempNum;
+}
+
+Node* SinglyLinearList::createNewNode() {
+	Node *newNode = new Node;
+	getData(newNode);
+	return newNode;
+}
+
+void SinglyLinearList::addNodeAtLast(Node *newNode) {
+	if (head == NULL)
+		head = newNode;
+	else {
+		Node *lastNode = getLastNode_EffectiveMethod();
+		lastNode->nextNode = newNode;
+	}
+}
+
+void SinglyLinearList::displayNode() {
+	Node *trav = head;
+	while (trav != NULL) {
+		cout << trav->num << " ";
+		trav = trav->nextNode;
+	}
+	cout << endl;
+}
+
+bool SinglyLinearList::isListEmpty() {
+	if (head == NULL)
+		return true;
+	else
+		return false;
+}
+
+int SinglyLinearList::getListLength(Node* &head) {
+	Node *trav = head;
+	int len = 0;
+	while (trav != NULL) {
+		len++;
+		trav = trav->nextNode;
+	}
+	return len;
+}
+
+Node* SinglyLinearList::getHead() {
+	return this->head;
+}
+
+void SinglyLinearList::setHead(Node* head) {
+	this->head = head;
+}
+
+void SinglyLinearList::deleteList() {
+	this->head = NULL;
+}
+
+void SinglyLinearList::displayReverseList(Node *trav){
+	if(trav == NULL){
+		cout<<"Empty List"<<endl;
+		return;
+	}
+	if(trav->nextNode != NULL){
+		displayReverseList(trav->nextNode);
+	}
+	cout<<trav->num<<" ";
+	return;
+}
+
+SinglyLinearList::~SinglyLinearList() {
+	delete head;
+}
---
+++ Assignment2/DS_Assign2_Q2/SinglyLinearList.h
@@ -0,0 +1,24 @@
+#include "Node.h"
+#ifndef SRC_SINGLYLINEARLIST_H_
+#define SRC_SINGLYLINEARLIST_H_
+
+class SinglyLinearList {
+private:
+	Node *head;
+public:
+	SinglyLinearList();
+	Node* getLastNode();
+	Node* getLastNode_EffectiveMethod();
+	void getData(Node* newNode);
+	Node* createNewNode();
+	void addNodeAtLast(Node *newNode);
+	void displayNode();
+	bool isListEmpty();
+	int getListLength(Node* &head);
+	void deleteList();
+	Node* getHead();
+	void setHead(Node* head);
+	void displayReverseList(Node *trav);
+	~SinglyLinearList();
+};
+#endif /* SRC_SINGLYLINEARLIST_H_ */
